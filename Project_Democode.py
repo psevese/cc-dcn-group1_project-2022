b7853eaceb08af45fe5651a7fe30a19e520e65ef
psevese <pspsevese@gmail.com>
2022-12-06 03:58:55 +1000

    third fail test

--- Project_Democode.py
+++ Project_Democode.py
@@ -1,14 +1,8 @@
-print("Group One Project Test")
-
-print("my Second commit")
 while True print('Hello world')
   File "<stdin>", line 1
     while True print('Hello world')
                    ^
 SyntaxError: invalid syntax
-
-
-print("The End..!")
 
  
     
