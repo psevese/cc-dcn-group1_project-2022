cb919e5a672c8e076f6391fced5e17e8e4bb3bd7
psevese <pspsevese@gmail.com>
2022-12-06 03:54:47 +1000

    My second fail test

--- Project_Democode.py
+++ Project_Democode.py
@@ -1,6 +1,11 @@
 print("Group One Project Test")
 
-("my Second commit")
+print("my Second commit")
+while True print('Hello world')
+  File "<stdin>", line 1
+    while True print('Hello world')
+                   ^
+SyntaxError: invalid syntax
 
 
 print("The End..!")
