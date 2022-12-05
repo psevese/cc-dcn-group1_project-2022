c4cf36a0673223bc1422eaa828cb2ca0ca04feff
psevese <pspsevese@gmail.com>
2022-12-06 04:02:11 +1000

    forth fail test

--- Project_Democode.py
+++ Project_Democode.py
@@ -1,8 +1,6 @@
-while True print('Hello world')
-  File "<stdin>", line 1
-    while True print('Hello world')
-                   ^
-SyntaxError: invalid syntax
+print("Group One Jenkins Project 2022")
+
+print(test)
 
  
     
